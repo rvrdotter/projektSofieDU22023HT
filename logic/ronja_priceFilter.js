47e19c1bc1a5e36d2d86197097539df251a00778
theoskogsberg <theo.skogsberg02@gmail.com>
2023-12-08 11:36:52 +0100

    deleted unnecessary filter.js

--- logic/ronja_priceFilter.js
+++
@@ -1,9 +0,0 @@
-function minMaxPriceFilter(array, min, max) {
-    let filteredArray = [];
-    for (let i = 0; i < array.length; i++) {
-        if (array[i].price <= max && array[i].price >= min) {
-            filteredArray.push(array[i]);
-        }
-    }
-    return filteredArray;
-}
