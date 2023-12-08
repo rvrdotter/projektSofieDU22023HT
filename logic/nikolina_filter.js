f13c08c6fa653b2cd57f06c25cd99e08c3b2ad30
theoskogsberg <theo.skogsberg02@gmail.com>
2023-12-08 11:38:26 +0100

    delete unnecessary filter.js

--- logic/nikolina_filter.js
+++
@@ -1,9 +0,0 @@
-function filter_nikolina(array, min_price, max_price) {
-    let filtered_array = [];
-    for (let shoe of array) {
-        if (shoe.price >= min_price && shoe.price <= max_price) {
-            filtered_array.push(shoe);
-        }
-    }
-    return array
-}
