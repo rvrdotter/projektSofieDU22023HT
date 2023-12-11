8075f872a1c2f85651af2abc4210466c517351b6
nikolinnav <vasic_nikolina@yahoo.com>
2023-12-11 11:26:10 +0100

    link a js file to index.html

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-render_structure(document.body);
+//render_structure(document.body);
+
+const structureElements = render_srtucture(document.body);
+
+render_country_filter(structureElements.filter);
