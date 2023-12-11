6d206d5f0c6881167fd24ab8cd2d42933427dde0
nikolinnav <vasic_nikolina@yahoo.com>
2023-12-11 14:31:27 +0100

    render country filter function

--- components/filter/origin_filter/origin_filter.js
+++ components/filter/origin_filter/origin_filter.js
@@ -1,9 +1,26 @@
 function render_country_filter(parent) {
+
     const container = document.createElement("div");
     container.id = "country_filter";
     parent.append(container);
 
+    let text_origin = document.createElement("div");
+    text_origin.innerHTML = `
+     <div id="text"> Tillverkningsland </div>
+     `;
+    container.appendChild(text_origin);
+
+
+
     for (let country of COUNTRIES) {
-        render_filter_element(container, country.name);
+        let tempContainer = document.createElement("div");
+        tempContainer.innerHTML = `
+          <input type="radio" id="radio"> </input>
+          `;
+        container.appendChild(tempContainer);
+
+        container.append(country.name);
+
+
     }
 }
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-//render_structure(document.body);
+const structureElements = render_structure(document.body);
 
-const structureElements = render_srtucture(document.body);
+
 
 render_country_filter(structureElements.filter);
