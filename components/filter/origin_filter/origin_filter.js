ef165c2f311dc3bc3d2b573e97ca3a09355265e5
nikolinnav <vasic_nikolina@yahoo.com>
2023-12-11 11:10:43 +0100

    create a container for country filter

--- components/filter/origin_filter/origin_filter.js
+++ components/filter/origin_filter/origin_filter.js
@@ -1,1 +1,9 @@
-function render_country_filter(parent) { }
+function render_country_filter(parent) {
+    const container = document.createElement("div");
+    container.id = "country_filter";
+    parent.append(container);
+
+    for (let country of COUNTRIES) {
+        render_filter_element(container, country.name);
+    }
+}
