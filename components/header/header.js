a6b183b12fc1bdee0e638818afd8304bced9e1fc
antun0x <143426088+antun0x@users.noreply.github.com>
2023-12-12 15:26:47 +0100

    Bugfix & Header finished

--- components/header/header.js
+++ components/header/header.js
@@ -1,6 +1,13 @@
 function render_header(header) {
     
     header.innerHTML = `
-    <h1>Test</h1>
+    <div>CHOOSE YOUR SHOES</div>
+    <img src=../../media/images/going.jpg>
+    <div> STRIDE WITH PRIDE: UNLEASH YOUR INNER <b>SHOE-TALITY</b></div>
+    <div class="bottomimg">
+        <img src=../../media/images/dancing_shoes.jpg>
+        <img src=../../media/images/dancing_shoes_2.jpg>
+        <img src=../../media/images/dancing_shoes_3.jpg>
+    </div>
 `;
 }
--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 
 const structure_containers = render_structure();
 render_header(structure_containers.header);
-render_header(structure_containers.aboutUs)
-render_header(structure_containers.info)
+render_aboutUs(structure_containers.aboutUs)
+render_info(structure_containers.info)
 
 // render_shoe_list(structure_containers.products);
