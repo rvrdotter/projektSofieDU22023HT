2b0fa5884da7fac63b97a870c366a6639ae34da0
nikolinnav <vasic_nikolina@yahoo.com>
2023-12-13 14:42:19 +0100

    update filter style

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const structureElements = render_structure(document.body);
 render_modell_filter(structureElements.filter);
-
-
 render_price_filters(structureElements.filter);
 render_country_filter(structureElements.filter);
