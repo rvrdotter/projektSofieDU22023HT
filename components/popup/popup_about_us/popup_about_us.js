d1dfc7a231ed1e5595ae828e885f2e511abba53a
emblalundqvist <embla@dotvoid.se>
2023-12-14 22:19:21 +0100

    updated code and css

--- components/popup/popup_about_us/popup_about_us.js
+++ components/popup/popup_about_us/popup_about_us.js
@@ -7,24 +7,25 @@ function create_popup_about_us(parent) {
 
     container_div_about_us.id = 'popup-container-about-us';
     content_div_about_us.id = 'popup-content-about-us';
+    content_div_about_us.innerHTML = `
+        <div id="team_photo"></div>
+        <div id="team_text">Meet the team!</div>
+    `;
 
     container_div_about_us.appendChild(content_div_about_us);
     parent.appendChild(container_div_about_us);
 
+    // popup göms vid klick, på containern
     container_div_about_us.addEventListener('click', function() {
         container_div_about_us.style.display = 'none';
     })
 }
 
 
+// div_button ska vara referensen div-knappen som visar popup-en, finns i structure att hämta
+
 function create_popup_about_us_image(div_button) {
     div_button.addEventListener('click', function() {
-
-        const popup_content_div = document.querySelector('#popup-content-about-us');
-        popup_content_div.innerHTML = `
-        <div></div>
-        `;
-
         const popup_container_div = document.querySelector('#popup-container-about-us');
         popup_container_div.style.display = 'flex';
     })
