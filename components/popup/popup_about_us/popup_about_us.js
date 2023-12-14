c95c9936ea99e1ab0188dc4b7a95f48841821707
emblalundqvist <embla@dotvoid.se>
2023-12-14 14:47:16 +0100

    created popup-function , container div forabout us)

--- components/popup/popup_about_us/popup_about_us.js
+++ components/popup/popup_about_us/popup_about_us.js
@@ -1,3 +1,10 @@
-function create_popup_about_us(){
-    
+function create_popup_about_us(parent){
+    const container_div_about_us = document.createElement('div');
+    const content_div_about_us = document.createElement('div');
+
+    container_div_about_us.id = 'popup-container-about-us';
+    content_div_about_us.id = 'popup-content-about-us';
+
+    container_div_about_us.appendChild(content_div_about_us);
+    parent.appendChild(container_div_about_us);
 }
