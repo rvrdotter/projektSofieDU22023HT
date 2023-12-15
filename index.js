42af5412db311a3f54db24e0086218d132c85861
AntonHansen <nilsfritz10@gmail.com>
2023-12-15 10:36:17 +0100

    final

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 const structure_containers = render_structure();
 
 // render_header(structure_containers.header);
-// render_aboutUs(structure_containers.aboutUs)
-// render_info(structure_containers.info)
+// render_aboutUs(structure_containers.aboutUs);
+// render_info(structure_containers.info);
 
 // render_shoe_list(structure_containers.products);
