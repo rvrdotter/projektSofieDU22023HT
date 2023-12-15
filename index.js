cd1da5a08da6e6a661f103a47f17a653dad13d71
theoskogsberg <theo.skogsberg02@gmail.com>
2023-12-15 10:55:03 +0100

    updates

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ render_modell_filter(structure_containers.filter);
 render_price_filters(structure_containers.filter);
 render_country_filter(structure_containers.filter);
 
-// render_header(structure_containers.header);
-// render_aboutUs(structure_containers.aboutUs);
-// render_info(structure_containers.info);
+render_header(structure_containers.header);
+render_aboutUs(structure_containers.aboutUs);
+render_info(structure_containers.info);
 
-// render_shoe_list(structure_containers.products);
+render_shoe_list(structure_containers.products);
