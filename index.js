e13a935a7831c72bb7d148077dd4bbd33aa1b3fe
emblalundqvist <embla@dotvoid.se>
2023-12-15 20:25:09 +0100

    updated position of about us popup

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ render_country_filter(structure_containers.filter);
 
 render_header(structure_containers.header);
 const aboutUsButton = render_aboutUs(structure_containers.aboutUs);
-create_popup_about_us(structure_containers.aboutUs);
+create_popup_about_us(document.body);
 create_popup_about_us_clickhandler(aboutUsButton);
 render_info(structure_containers.info);
 render_shoe_list(structure_containers.products);
