a38b601526d6635fdc10dc751e0e15514a64b27b
AntonHansen <nilsfritz10@gmail.com>
2023-12-15 10:46:27 +0100

    Header footer image paths fixed

--- components/header/header.js
+++ components/header/header.js
@@ -2,12 +2,12 @@ function render_header(header) {
     
     header.innerHTML = `
     <div>CHOOSE YOUR SHOES</div>
-    <img src=../../media/images/going.jpg>
+    <img src="../../media/images/going.jpg">
     <div> STRIDE WITH PRIDE: UNLEASH YOUR INNER <b>SHOE-TALITY</b></div>
     <div class="bottomimg">
-        <img src=../../media/images/dancing_shoes.jpg>
-        <img src=../../media/images/dancing_shoes_2.jpg>
-        <img src=../../media/images/dancing_shoes_3.jpg>
+        <img src="../../media/images/dancing_shoes.jpg">
+        <img src="../../media/images/dancing_shoes_2.jpg">
+        <img src="../../media/images/dancing_shoes_3.jpg">
     </div>
 `;
 }
